8dc19dc1a0bc5ee4458c3f44fe5c6633572a8c00
Kairos <daniel@famroepke.nl>
2023-06-03 16:08:29 +0200

    Initial commit

---
+++ portfolio.js
@@ -0,0 +1,23 @@
+let portfolio = [
+    {
+        "name": "Arcanion",
+        "description": "A Minecraft plugin developed using Kotlin. It adds magic, spells, and custom items.",
+        "link": "https://github.com/The-Goat-Kairos",
+        "icon": "phoenix",
+        "type": ["minecraft","kotlin"]
+    },
+    {
+        "name": "World's worst input",
+        "description": "The world's worst phone number inputter ever.",
+        "link": "https://daniel.coderschool.nl/TerriblePhoneNumber/index.php",
+        "icon": "",
+        "type": "site"
+    }, 
+    {
+        "name": "Galleria",
+        "description": "A gallery made for school using a template using php.",
+        "link": "https://daniel.coderschool.nl/NiceAdmin/pages-register.php",
+        "icon": "bootstrap",
+        "type": "school"
+    },
+]
